610c0f8761042e0159787f6b898f21ece0d773b2
saiani1 <saiani@naver.com>
2023-10-25 15:57:31 +0900

    feat: todo수정 기능 구현 완료

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
-defineProps<{
+import { ref } from 'vue'
+const Props = defineProps<{
   content: string
   checked: Boolean
 }>()
 
+const isModify = ref(false)
+const itemContent = ref(Props.content)
+
 const emits = defineEmits(['delItem'])
+
+const handleClickModifyBtn = () => {
+  isModify.value = !isModify.value
+}
 
 const handleClickDelBtn = (e: MouseEvent) => {
   const target = e.target as HTMLButtonElement
@@ -17,9 +25,19 @@ const handleClickDelBtn = (e: MouseEvent) => {
   <div class="todoItemWrap">
     <div class="leftWrap">
       <input type="checkbox" :value="checked" />
-      <p class="todoText">{{ content }}</p>
+      <template v-if="!isModify">
+        <p class="todoText">{{ itemContent }}</p>
+      </template>
+      <template v-else>
+        <input type="text" v-model="itemContent" />
+      </template>
     </div>
-    <button type="button" class="delBtn" :name="content" @click="handleClickDelBtn">X</button>
+    <div class="btnWrap">
+      <button type="button" class="modifyBtn" @click="handleClickModifyBtn">
+        {{ !isModify ? '수정' : '저장' }}
+      </button>
+      <button type="button" class="delBtn" :name="content" @click="handleClickDelBtn">X</button>
+    </div>
   </div>
 </template>
 
